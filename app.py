50cf5aa84e269b33f0beaf790426e935c331cf39
abbey <abbeyyacoe@gmail.com>
2021-03-30 17:03:25 +0200

    update comments

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# Open Sanitized Text based on the Maxims
+# Open text file I organized from Project Gutenberg
 # https://www.gutenberg.org/files/9105/9105-h/9105-h.htm
 f = open("roch.txt", "r")
 a_list = f.read().strip('').split('\n')
@@ -16,10 +16,9 @@ a_list = f.read().strip('').split('\n')
 filter_object = filter(lambda x: x != "", a_list)
 without_empty_strings = list(filter_object)
 
-# generate random number from list
+# generate random number from the length of the list
 random_number = randrange(len(without_empty_strings))
 
 
-# Create a tweet.
-# Calls a random number in the list
+# Selects a random line of text from the list and tweets it.
 api.update_status(without_empty_strings[random_number])
